64e357c8afd1e041cd55c19c51b78c2876c35f34
Yuvraj Verma <62199410+HeyYuvraj@users.noreply.github.com>
2023-11-03 10:39:43 +0530

    update

--- Demo10/src/main/java/com/scaleupindia/enums/PetType.java
+++
@@ -1,9 +0,0 @@
-package com.scaleupindia.enums;
-
-/**
- * @author abhishekvermaa10
- *
- */
-public enum PetType {
-	BIRD, CAT, DOG, FISH, RABBIT
-}
--- Demo15/src/main/java/com/scaleupindia/enums/Gender.java
+++
@@ -1,9 +0,0 @@
-package com.scaleupindia.enums;
-
-/**
- * @author abhishekvermaa10
- *
- */
-public enum Gender {
-	M, F
-}
